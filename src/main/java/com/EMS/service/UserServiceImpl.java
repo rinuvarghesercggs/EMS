a213ea0545284967b4051cd1dbb99b2cfffa0d72
charlie <charlie@titech-OptiPlex-3010>
2019-03-05 10:34:12 +0530

    user service changes

--- src/main/java/com/EMS/service/UserServiceImpl.java
+++ src/main/java/com/EMS/service/UserServiceImpl.java
@@ -4,16 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.EMS.model.UserModel;
-import com.EMS.repository.UserRepositary;
+import com.EMS.repository.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
 	@Autowired
-	UserRepositary userRepositary;
+	UserRepository userRepository;
 	
 	@Override
 	public UserModel getUserDetailsById(Long id){
-		return  userRepositary.getOne(id);
+		return  userRepository.getOne(id);
 				
 	}
 
