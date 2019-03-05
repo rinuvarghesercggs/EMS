8aff7500e65c603e364953c426d457bd3702ba9d
haritha <haritha@titech-OptiPlex-3010>
2019-03-05 12:46:06 +0530

    change in query

--- src/main/java/com/EMS/repository/ProjectRepository.java
+++ src/main/java/com/EMS/repository/ProjectRepository.java
@@ -19,8 +19,8 @@ public interface ProjectRepository extends JpaRepository<ProjectModel, Long> {
 	@Query("SELECT s.id,s.projectName FROM ProjectModel s")
 	List<Object[]>getByIdName();
 
-	@Query("SELECT count(p) FROM ProjectModel p WHERE p.projectName LIKE :name")
-	int findproject(@Param("name") String getprojectName);
+	@Query("SELECT count(p) FROM ProjectModel p WHERE p.projectName=?1")
+	int findproject(String getprojectName);
 
 	
 
