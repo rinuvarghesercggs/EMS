65bcbbd085ee62ea10df5293349fb83c8f3c3d47
haritha <haritha@titech-OptiPlex-3010>
2019-03-05 14:22:33 +0530

    change in url

--- src/main/java/com/EMS/controller/LoginController.java
+++ src/main/java/com/EMS/controller/LoginController.java
@@ -5,6 +5,7 @@ import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import com.EMS.service.LoginService;
 
 
 @RestController
+@RequestMapping(value="/login")
 public class LoginController {
 
 	@Autowired
