670555c3578ace3ff36bed71570c9c35d95a26f6
jinu <jinu.shaji@rcggs.com>
2019-06-06 18:25:28 +0530

    Super Lead Bug Fixes

--- src/main/java/com/EMS/repository/UserRepository.java
+++ src/main/java/com/EMS/repository/UserRepository.java
@@ -16,7 +16,7 @@ public interface UserRepository extends JpaRepository<UserModel, Long>{
 //	@Query("SELECT u FROM UserModel u WHERE u.userName=?1 AND u.active = true") 
 	UserModel getUserdetails(String userName, String password);
 
-	@Query("SELECT u FROM UserModel u WHERE u.role in (2,3)")//for getting user details with role as owner by providing role
+	@Query("SELECT u FROM UserModel u WHERE u.role in (2,5)")//for getting user details with role as owner by providing role
 	List<UserModel> getProjectOwners();
 
 	@Query("SELECT u FROM UserModel u WHERE u.role in(2,3,4,5) AND u.active = true order by firstName")
