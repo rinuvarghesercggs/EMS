9a525a4571b5e673cf0b39a50447e171083655da
Nisha Thomas <nisha.thomas@titechnologies.org>
2019-08-29 14:14:18 +0530

    unauthorized token error message

--- src/main/java/com/EMS/security/jwt/JwtTokenFilter.java
+++ src/main/java/com/EMS/security/jwt/JwtTokenFilter.java
@@ -1,5 +1,7 @@
 package com.EMS.security.jwt;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
@@ -10,6 +12,8 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JwtTokenFilter extends GenericFilterBean {
 
@@ -35,8 +39,24 @@ public class JwtTokenFilter extends GenericFilterBean {
 			 
 			filterChain.doFilter(req, res);
 		} catch (InvalidJwtAuthenticationException eje) {
-	        ((HttpServletResponse) res).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+	        //((HttpServletResponse) res).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			//FilterErrorResponse errorResponse = new FilterErrorResponse(e);
+			final Map<String, Object> mapBodyException = new HashMap<>() ;
+			mapBodyException.put("status"    , "Failed") ;
+			mapBodyException.put("message"  , "Unauthorized token") ;
+			((HttpServletResponse) res).setStatus(HttpServletResponse.SC_OK);
+			//((HttpServletResponse) res).getWriter().write(convertObjectToJson(HttpServletResponse.SC_UNAUTHORIZED));
+			final ObjectMapper mapper = new ObjectMapper() ;
+			mapper.writeValue(res.getOutputStream(), mapBodyException) ;
 	    }
+	}
+
+	private String convertObjectToJson(Object object) throws JsonProcessingException {
+		if (object == null) {
+			return null;
+		}
+		ObjectMapper mapper = new ObjectMapper();
+		return mapper.writeValueAsString(object);
 	}
 
 }
